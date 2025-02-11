2d445b01d21c7bebb6aaf648a997cd61089182b8
jkh <jkh@example.com>
2025-02-11 22:15:05 +0900

    debugging

--- api/tests/test_composer.py
+++ api/tests/test_composer.py
@@ -1,43 +1,38 @@
 # tests/test_composer.py
 import pytest
 from ..models import Composer
-from .utils import create_api_test, list_api_test
-
-
-@pytest.fixture
-def test_data():
-    name_a = "Beethoven"
-    name_b = "Mozart"
-    full_name_a = "Ludwig van Beethoven"
-    full_name_b = "Wolfgang Amadeus Mozart"
-
-    return {
-        "normal": {
-            "name": name_a,
-            "full_name": full_name_a,
-        },  # 정상 데이터
-        "unique": {
-            "name": name_a,
-            "full_name": full_name_b,
-        },  # name 필드는 중복 불가
-        "duplicate": {
-            "name": name_b,
-            "full_name": full_name_a,
-        },  # full_name 필드는 중복 가능
-        "expected": {
-            "use_page": False,
-            "cnt": 1,
-            "field": "name",
-            "value": name_a,
-        },
-    }
+from .utils import test_post_api  # , test_get_api, test_get_sort
 
 
 @pytest.mark.django_db
 class TestComposerAPI:
-    def test_create_composer(self, api_client, test_data):
-        create_api_test(api_client, Composer, "composer-list", test_data)
+    def test_post(self):
+        test_data = {
+            "normal": {
+                "name": "Beethoven",
+                "full_name": "Ludwig van Beethoven",
+            },  # 정상 데이터
+            "unique": {
+                "name": "Beethoven",
+                "full_name": "Wolfgang Amadeus Mozart",
+            },  # name 필드는 중복 불가
+            "duplicate": {
+                "name": "Mozart",
+                "full_name": "Ludwig van Beethoven",
+            },  # full_name 필드는 중복 가능
+        }
+        test_post_api(test_data)
+        # test_post_api(api_client, Composer, "composer-list", test_data)
 
-    def test_get_composers(self, api_client, test_data):
-        Composer.objects.create(**test_data["normal"])
-        list_api_test(api_client, "composer-list", test_data["expected"])
+    # def test_get(self, api_client):
+    #     Composer.objects.create(name="Mozart", full_name="Wolfgang Amadeus Mozart")
+    #     expected_data = {
+    #         "use_page": False,
+    #         "cnt": 1,
+    #         "field": "name",
+    #         "value": "Mozart",
+    #     }
+    #     test_get_api(api_client, "composer-list", expected_data)
+
+    #     Composer.objects.create(name="Beethoven", full_name="Ludwig van Beethoven")
+    #     test_get_sort(api_client, "composer-list", expected_data)
--- api/tests/utils.py
+++ api/tests/utils.py
@@ -1,30 +1,43 @@
 # tests/utils.py
 import pdb  # ✅ 디버거 추가
 from django.urls import reverse
+from rest_framework.test import APIClient
+from ..models import Composer
 
 
-def create_api_test(api_client, model, url_name, data):
-    """POST 요청을 보내어 객체를 생성하는 API 테스트 함수"""
-    url = reverse(url_name)
-    create_api_request(api_client, model, url, data["normal"], 201, 1)  # 정상
-    create_api_request(api_client, model, url, data["unique"], 400, 1)  # 중복 불가
-    create_api_request(api_client, model, url, data["duplicate"], 201, 2)  # 중복 가능
+def test_post_api(data):
+    api_client = APIClient()
+
+    url = reverse("composer-list")
+    test_model = Composer
+    request_post(api_client, test_model, url, data["normal"], 201, 1)  # 정상
+    request_post(api_client, test_model, url, data["unique"], 400, 1)  # 중복 불가
+    request_post(api_client, test_model, url, data["duplicate"], 201, 2)  # 중복 가능
 
 
-def list_api_test(api_client, url_name, expected_data):
-    """GET 요청을 보내어 목록 API 테스트 함수"""
-    url = reverse(url_name)
-    response = api_client.get(url)
-    assert response.status_code == 200
-
-    # 페이지네이션을 사용 여부에 따라 결과 데이터 구조 다름
-    res = response.data["results"] if expected_data["use_page"] else response.data
-    assert len(res) == expected_data["cnt"]
-    assert res[0][expected_data["field"]] == expected_data["value"]
-
-
-def create_api_request(api_client, model, url, data, code, cnt):
-    """POST 요청을 보내어 객체를 생성하는 API 요청 함수"""
+def request_post(api_client, model, url, data, code, cnt):
     response = api_client.post(url, data, format="json")
     assert response.status_code == code
     assert model.objects.count() == cnt
+
+
+# def test_get_api(api_client, url_name, expected_data):
+#     url = reverse(url_name)
+#     response = api_client.get(url)
+#     assert response.status_code == 200
+
+#     # 페이지네이션을 사용 여부에 따라 결과 데이터 구조 다름
+#     res = response.data["results"] if expected_data["use_page"] else response.data
+#     assert len(res) == expected_data["cnt"]
+#     assert res[0][expected_data["field"]] == expected_data["value"]
+
+
+# def test_get_sort(api_client, url_name, expected_data):
+#     url = reverse(url_name)
+#     response = api_client.get(url)
+#     assert response.status_code == 200
+
+#     # 페이지네이션을 사용 여부에 따라 결과 데이터 구조 다름
+#     res = response.data["results"] if expected_data["use_page"] else response.data
+#     res_list = [item[expected_data["field"]] for item in res]
+#     assert res_list == sorted(res_list)
